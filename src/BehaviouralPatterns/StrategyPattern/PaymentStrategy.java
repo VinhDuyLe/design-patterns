7ad855dee50a5324ddb8d335bcf875b301f3e8a5
vinhduyle <leduyvinh91@gmail.com>
2025-08-05 12:24:56 -0700

    strategy pattern

---
+++ src/BehaviouralPatterns/StrategyPattern/PaymentStrategy.java
@@ -0,0 +1,6 @@
+package BehaviouralPatterns.StrategyPattern;
+
+public interface PaymentStrategy {
+    void pay(int amount);
+}
+
---
+++ src/BehaviouralPatterns/StrategyPattern/ShoppingCart.java
@@ -0,0 +1,15 @@
+package BehaviouralPatterns.StrategyPattern;
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(int amount) {
+        if (paymentStrategy == null) {
+            throw new IllegalStateException("Payment strategy not set");
+        }
+        paymentStrategy.pay(amount);
+    }
+}
